29f3d91303b244414a3739594f74911741d97ec0
Agusmfth <agusmiftach2013@gmail.com>
2025-06-04 00:45:04 +0700

    Update contact section with new layout and animations

--- script.js
+++ script.js
@@ -6,53 +6,48 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
+let header = document.querySelector('.header');
 
 window.onscroll = () => {
+    let top = window.scrollY;
+
     sections.forEach(sec => {
-        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
             });
-        };
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
     });
 
-
-let header = document.querySelector('.header');
-
-header.classList.toggle('sticky', window.scrollY > 100);
-
-
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active');
-
+    header.classList.toggle('sticky', top > 100);
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
-
+// Swiper
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 50,
     loop: true,
     grabCursor: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });
 
-
+// Dark Mode
 let darkModeIcon = document.querySelector('#darkMode-icon');
 
 darkModeIcon.onclick = () => {
@@ -60,7 +55,7 @@ darkModeIcon.onclick = () => {
     document.body.classList.toggle('dark-mode');
 };
 
-
+// Scroll Reveal
 ScrollReveal({
     distance: '80px',
     duration: 2000,
